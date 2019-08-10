1b15742161041e73f74c0d19b4415410b33dc7c9
yamatomatsuda <yamato19990708@gmail.com>
2019-08-10 21:05:21 +0900

    add control_system.py this function won't work

--- control_system.py
+++ control_system.py
@@ -11,6 +11,7 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 
+from rclpy.executors import MultiThreadedExecutor
 from std_msgs.msg import String
 
 
@@ -21,26 +22,27 @@ class Control_System(Node):
         self.create_subscription(
             String,
             'control_system/command',
-            self.command_callback,
-            qos_profile_sensor_data
+            qos_profile_sensor_data,
+            self.command_callback
         )
 
         self.create_subscription(
             Odometry,
             'odom',
             self.now_angular,
-            qos_profile_sensor_data
+            qos_profile=qos_profile_sensor_data
         )
-
         self.pub_vel = self.create_publisher(
             Twist,
             'cmd_vel',
             qos_profile_sensor_data
         )
+        self.angular = 0.0
 
-    def now_angular(self, massage):
-        self.angular = math.degrees(
-            2*math.asin(massage.pose.pose.orientation.z))
+    def now_angular(self, message):
+        prinentation_z = message.pose.pose.orientation.z
+        self.angular = math.degrees(2 * math.asin(prinentation_z))
+        print(self.angular)
 
     def command_callback(self, msg):
 
@@ -48,7 +50,7 @@ class Control_System(Node):
         command = msg.data.split(',')
 
         if 'rotate' == command[0].replace('Command:', ''):
-            self.rotate(command[1].replace('Content:', ''))
+            self.rotate(float(command[1].replace('Content:', '')))
 
     def cerebrum_publisher(self, message):
         self.senses_publisher = self.create_publisher(
@@ -60,36 +62,39 @@ class Control_System(Node):
         self.senses_publisher.publish(_trans_message)
         self.destroy_publisher(self.senses_publisher)
 
-    def rotate(self, angular):
-        angular = int(angular)
-        _acceleration = 0
-        _velocity = 0
+    def rotate(self, goal_angular):
+        print('rotate')
 
-        difference, direction = calculate_speed.space_time_convert(angular)
+        privious_angular = self.angular
 
-        flag = 1
+        operator = 0
+        flag = True
 
-        while flag == 1:
-            _velocity,
-            _acceleration,
-            flag = calculate_speed.calculate_next(
-                difference,
-                direction,
-                _velocity,
-                _acceleration
-            )
-            vel = Twist()
-            vel.linear.x = 0
-            vel.angular.z = _velocity*direction
-            self.pub_vel.publish(vel)
-        self.cerebrum_publisher('Return:1,Content:None')
+        
+        while flag is True:
+            operator = calculate_speed.convertor(self.angular, privious_angular, operator)
+            if operator == 1:
+                velocity = calculate_speed.differencer(goal_angular, self.angular+360.0)
+            elif operator == -1:
+                velocity = calculate_speed.differencer(goal_angular, self.angular-360.0)
+            else:
+                velocity = calculate_speed.differencer(goal_angular, self.angular)
+            print(self.angular)
+            if velocity == 0:
+                print('finish')
+                flag = False
+            velocity_message = Twist()
+            velocity_message.linear.x = 0.0
+            velocity_message.angular.z = velocity
+            self.pub_vel.publish(velocity_message)
 
 
 def main():
     rclpy.init()
+    executor = MultiThreadedExecutor()
     node = Control_System()
-    rclpy.spin(node)
-    node.destroy_node()
+    executor.add_node(node)
+    rclpy.spin()
     rclpy.shutdown()
 
 
